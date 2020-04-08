a4452ff84e6ab9c5c217d049ddceb3b63405da9a
Eren Erdogan <eren.erdogan@stud.fh-campuswien.ac.at>
2020-04-08 14:16:25 +0200

    add RestExample for Post

--- RestConnectExample/index.js
+++ RestConnectExample/index.js
@@ -1,9 +1,9 @@
 
 async function getResponse(){
-    var myJson = await getTestResponseJson();
+    var myJson = await getTestGetResponseJson();
     console.log(myJson);
 }
-async function getTestResponseJson(){
+async function getTestGetResponseJson(){
         const response = await fetch('https://localhost:8443/auth', {
           method: 'GET',
           headers: {
@@ -13,4 +13,14 @@ async function getTestResponseJson(){
         const myJson = await response.json(); //extract JSON from the http response
         return myJson;
 }
-
+async function getTestPostResponseJson(){
+    const response = await fetch('https://localhost:8443/auth', {
+    method: 'POST',
+    body: '', //TODO create request body
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+  const myJson = await response.json(); //extract JSON from the http response
+  return myJson;
+}
